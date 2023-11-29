ae556bda803d1bb91546b6f539d512dec39a40be
Natxo <natxetepala@gmail.com>
2023-11-29 16:59:51 +0100

    Automatic Update

---
+++ CS2ServerCreator - copia/About.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS2ServerCreator
+{
+    public partial class About : Form
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void labelGitHub_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Natxo09");
+        }
+    }
+}
